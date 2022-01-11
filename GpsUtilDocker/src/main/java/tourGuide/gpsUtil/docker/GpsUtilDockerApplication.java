88f028d858add007d1b701d73cf6ae4b16b2b99d
Kafeinedev <kafeinedev@protonmail.com>
2022-01-11 06:41:20 +0100

    Setting up dockerisation project for gpsutil

---
+++ GpsUtilDocker/src/main/java/tourGuide/gpsUtil/docker/GpsUtilDockerApplication.java
@@ -0,0 +1,13 @@
+package tourGuide.gpsUtil.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsUtilDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsUtilDockerApplication.class, args);
+	}
+
+}
